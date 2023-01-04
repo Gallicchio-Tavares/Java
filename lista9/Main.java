ec718f01361ccf79904ce106b9b709eaa65e4ee1
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2023-01-04 00:07:46 -0300

    Update Main.java

--- lista9/Main.java
+++ lista9/Main.java
@@ -1,7 +1,10 @@
 package lista9;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args){
@@ -19,23 +22,39 @@ public class Main {
         a4.setNota(7);
 
         ArrayList<Aluno> escola = new ArrayList<Aluno>();
+        Map<String, Aluno> escola2 = new HashMap<String, Aluno>();
         escola.add(a1);
         escola.add(a2);
         escola.add(a3);
         escola.remove(2); // removi a3 (índice 2)
         escola.add(a4);
 
-        System.out.println(escola.get(0).getNome());
+        //System.out.println(escola.get(0).getNome());
         System.out.println(Utils.existe(escola, a3)); // vai sair false
-        System.out.println(escola);
-        System.out.println("----------------------------------------------------");
+        System.out.println(a1);
+        System.out.println("-----------------------------------------------------");
         Utils.ordena(escola);
         System.out.println(escola);
+        System.out.println("-----------------------------------------------------");
+        try{
+           Set conjuntoAlunos = getAlunos();
+            escola2 = Utils.retornaDados(conjuntoAlunos);
+            System.out.println(escola2); 
+        } catch(exceptionUtils e){
+            System.out.println(e.getMessage()); // msg da exception
+            e.printStackTrace(); // vai aparecer a pilha de erro
+        }
+        
+
     }
-    public static List getAlunos(){
-        ArrayList<String> x = new ArrayList<String>();
+    public static Set getAlunos(){
+        Set<String> x = new HashSet<String>();
         x.add("154-3#Simba Silva#3.7#R"); // id#nome#nota#tipo
         x.add("553-2#Juju Juba#10.0#E");
+        x.add("095-1#Sally Face#7.8#R");
+        x.add("112-5#John Wick#9.5#E");
+        x.add("666-0#Lucifer Morningstar#9.0#E");
+        //       0      1       2   3
 
         return x; 
     }
