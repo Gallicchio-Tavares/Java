ec0afb85f679d2745f1ab457049e86d7109a2274
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2023-01-05 14:40:27 -0300

    Update ProgramaPrincipal.java

--- lista9/ProgramaPrincipal.java
+++ lista9/ProgramaPrincipal.java
@@ -1,17 +1,12 @@
 package lista9;
 import java.util.Scanner;
 import java.util.Iterator;
-import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-/*
- * O programa deverá receber do console 100  strings  no  seguinte  formato:  id#nome#nota#tipo  
- * (i.e.,  mesmo  formato  explicado  no  item  E).  Utilizando os métodos já criados, exiba no console os
- *  nomes e notas dos alunos com os nomes ordenados em ordem alfabética.
- */
 
 /*
         x.add("154-3#Rodrick Heffley#3.7#R"); // id#nome#nota#tipo
@@ -27,7 +22,7 @@ public class ProgramaPrincipal{
         Map<String, Aluno> escola = new HashMap<String, Aluno>();
         Set<String> conjuntoAlunos = new HashSet<String>();
         
-        try{
+        try{ // tá funcionando
             System.out.println("Insira as informações no formato id#nome#nota#tipo:");
             for(int i = 0; i < 5; i++){ // primeiro treinar com 5
                 conjuntoAlunos.add(sc.nextLine());
@@ -38,17 +33,15 @@ public class ProgramaPrincipal{
             e.printStackTrace(); // vai aparecer a pilha de erro
         }
         
-        //Utils.ordena(escola.values());
-        //TO-DO: descobrir como ordenar essa bosta de hashmap se a questão pede lista CARALHO
+        List<Aluno> lista = new ArrayList<Aluno>(escola.values());
+        Utils.ordena(lista); 
         System.out.println(" ");
 
-        Collection c = escola.values();
-        Iterator it = c.iterator();
+        Iterator<Aluno> it = lista.iterator();
         while(it.hasNext()){
             Aluno o = (Aluno)it.next();
             System.out.println(o);
         }
-        
         sc.close();
     }
 }
